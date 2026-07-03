5095f235d6999ad4d04c3f4940ea0a13d31122f8
preesha2010 <preesham2010@gmail.com>
2026-07-03 12:20:58 +0530

    table ui changes

--- dashboard.py
+++ dashboard.py
@@ -282,7 +282,7 @@ def download_report(app_name, scan_id):
         if n == 5:
             cw = [W*0.18, W*0.12, W*0.14, W*0.20, W*0.37]
         elif n == 6:
-            cw = [W*0.16, W*0.11, W*0.11, W*0.10, W*0.17, W*0.35]
+            cw = [W*0.16, W*0.12, W*0.13, W*0.11, W*0.17, W*0.30]
         else:
             cw = [W/n]*n
 
